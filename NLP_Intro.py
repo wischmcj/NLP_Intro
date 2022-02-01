8c2b355ef775408f38d883df8b4aad57c42e2690
wischmcj <wischmcj@miamioh.edu>
2022-02-01 00:29:35 -0500

    Fi idt
    Adding in term frequency and idt.
    added lemma tokenization and stop word removal

--- NLP_Intro.py
+++ NLP_Intro.py
@@ -1,7 +1,18 @@
 
 import numpy as np # linear algebra
 import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
+import nltk
 from sklearn import feature_extraction, linear_model, model_selection, preprocessing
+from sklearn.feature_extraction.text import TfidfVectorizer, TfidfTransformer
+from sklearn.feature_extraction.text import CountVectorizer
+
+from nltk.stem import WordNetLemmatizer
+from nltk.tokenize import RegexpTokenizer
+from sklearn.metrics.pairwise import linear_kernel
+from nltk import word_tokenize          
+from nltk.stem import WordNetLemmatizer
+import nltk
+from nltk.corpus import stopwords
 
 train_df = pd.read_csv("./kaggle/input/train.csv")
 test_df = pd.read_csv("./kaggle/input/test.csv")
@@ -14,6 +25,7 @@ test_df = pd.read_csv("./kaggle/input/test.csv")
 
 count_vectorizer = feature_extraction.text.CountVectorizer()
 
+train_vectors = count_vectorizer.fit_transform(train_df["text"])
 # Vectorizer test 
 # example_train_vectors = count_vectorizer.fit_transform(train_df["text"][0:5])
 
@@ -21,23 +33,23 @@ count_vectorizer = feature_extraction.text.CountVectorizer()
 # print(example_train_vectors[0].todense())
 # print(example_train_vectors[0])
 
-# Converte text column to feature array
-train_vectors = count_vectorizer.fit_transform(train_df["text"])
-
-print(train_vectors)
+# Convert text column to feature array
 ## note that we're NOT using .fit_transform() here. Using just .transform() makes sure
 # that the tokens in the train vectors are the only ones mapped to the test vectors - 
 # i.e. that the train and test vectors use the same set of tokens.
 test_vectors = count_vectorizer.transform(test_df["text"])
+
+#print(test_vectors)
+
 clf = linear_model.RidgeClassifier()
-
 # Calculate model accuracy with various subsets of the labled data  - F1 score 
-
 scores = model_selection.cross_val_score(clf, train_vectors, train_df["target"], cv=3, scoring="f1")
-print(scores)
+#print(scores)
+
 
 
  #TF idf to improve scores 
+ #https://docs.scipy.org/doc/scipy/reference/generated/scipy.sparse.csr_matrix.html
  
 def computeTF(wordDict, bagOfWords):
     tfDict = {}
@@ -47,26 +59,77 @@ def computeTF(wordDict, bagOfWords):
     return tfDict
 
 
-train_df_text = train_df["text"]
-train_df_bow = train_df_text
-for x in train_df_text:
-    i=0
-    train_df_bow[i] = x.split(' ')
-    i=i+1
-    
-print(train_vectors[1].todense())
+train_df_bow =train_df["text"]
+ 
+
+# for x in train_df["bow"]:
+#     i=0
+#     #train_df[i,'BOW'] = 
+#     print(type(x.split(' ')))
+#     #i=i+1
+ 
+def preprocess_text(document):
+    for text in document:
+   # Tokenise words while ignoring punctuation
+        tokeniser = RegexpTokenizer(r'\w+')
+        tokens = tokeniser.tokenize(text)
+     
+    return tokens
+ 
+stop_words = set(stopwords.words('english')) 
+#https://gist.github.com/4OH4/f727af7dfc0e6bb0f26d2ea41d89ee55       
+class LemmaTokenizer:
+    ignore_tokens = [',', '.', ';', ':', '"', '``', "''", '`']
+    def __init__(self):
+        self.wnl = WordNetLemmatizer()
+    def __call__(self, doc):
+        return [self.wnl.lemmatize(t) for t in word_tokenize(doc) if t not in stop_words]
 
 
-#def computeTF(train_vectors, bagOfWords):
+tokenizer=LemmaTokenizer()
+token_stop = tokenizer(' '.join(stop_words))
 
 
+# bow_df = pd.DataFrame().values.tolist()
+
+# for x in train_df["text"]:
+#     i=0
+# #print(preprocess_text(x))
+#     bow_df.insert(i, x.split())
+#     i=i+1
+  
+
+tfIdfVectorizer=TfidfVectorizer(use_idf=True,stop_words=token_stop, 
+                              tokenizer=tokenizer )
+tfIdf = tfIdfVectorizer.fit_transform(train_df["text"])
+df = pd.DataFrame(tfIdf[0].T.todense(), index=tfIdfVectorizer.get_feature_names(), columns=["TF-IDF"])
+df = df.sort_values('TF-IDF', ascending=False)
+print (df.head(25))
 
 
-# ##PRedict answers based on model
-# clf.fit(train_vectors, train_df["target"])
+# countVectorizer = CountVectorizer()
+# tfIdfTransformer = TfidfTransformer(use_idf=True)
+# wordCount = countVectorizer.fit_transform(train_df["text"])
+# newTfIdf = tfIdfTransformer.fit_transform(wordCount)
 
-# ##Submit
-# sample_submission = pd.read_csv("./kaggle/submission.csv")
-# sample_submission["target"] = clf.predict(test_vectors)
-# print(sample_submission.head())
+# df = pd.DataFrame(newTfIdf[0].T.todense(), index=countVectorizer.get_feature_names(), columns=["TF-IDF"])
+
+# df = df.sort_values('TF-IDF', ascending=False)
+# print (df.head(25))
+
+# #https://towardsdatascience.com/tf-idf-explained-and-python-sklearn-implementation-b020c5e83275
+
+# tfIdfVectorizer=TfidfVectorizer(use_idf=True)
+# tfIdf = tfIdfVectorizer.fit_transform(train_df["text"])
+# df = pd.DataFrame(tfIdf[0].T.todense(), index=tfIdfVectorizer.get_feature_names(), columns=["TF-IDF"])
+# df = df.sort_values('TF-IDF', ascending=False)
+# print (df.head(25))
+
+# def computeTF(train_vectors, bagOfWords):
+#  ##PRedict answers based on model
+#  clf.fit(train_vectors, train_df["target"])
+#  ##Submit
+#  #sample_submission = pd.read_csv("./kaggle/submission.csv")
+# # sample_submission["target"] = clf.predict(test_vectors)
+# # print(sample_submission.head())
 
